ae480b6b3286630223a0573f78bd242f77ff86a0
Kevin Nguyen <khoa01004@gmail.com>
2021-12-08 13:57:13 -0500

    edit lại spacing cho dễ đọc

--- Wifi/src/colamthimoicoan/Main.java
+++ Wifi/src/colamthimoicoan/Main.java
@@ -1,4 +1,5 @@
 package colamthimoicoan;
+
 /* 
 make sure the cables between the router & nodes are plugge din firmly
 "Did that fix the problem?"
@@ -8,46 +9,40 @@ get a new router
 */
 import java.util.Scanner;
 
-public class Main
-{
-public static void main(String[] args)
-{
-int input;
-try (Scanner keyboard = new Scanner(System.in)) {
-System.out.println("Try rebooting the computer and try to connect");
-      
-System.out.println(" Did that fix the problem? 1 for yes 2 for no");
-      
-input = keyboard.nextInt(); 
-if (input == 2)
-{
-System.out.print("reboot the router and try to reconnect");
-      
-System.out.println(" Did that fix the problem? 1 for yes 2 for no");
-input = keyboard.nextInt();
-if (input == 2)
-{
-System.out.print("Make sure the cables between routers and nodes are plugged in firmly");
-           
-System.out.println(" Did that fix the problem? 1 for yes 2 for no");
-input = keyboard.nextInt();
-if (input == 2)
-{
-System.out.print(" Move router to new location");
-                 
-System.out.println(" Did that fix the problem? 1 for yes 2 for no");
-input = keyboard.nextInt();
-if (input == 2)
-{
-System.out.print(" Get a new router");
-}
-if (input == 1 || input == 1)
-System.exit(0);
-                   
-}
-}
-      
-}
-}
-}
+public class Main {
+    public static void main(String[] args) {
+        int input;
+        try (Scanner keyboard = new Scanner(System.in)) {
+            System.out.println("Try rebooting the computer and try to connect");
+
+            System.out.println(" Did that fix the problem? 1 for yes 2 for no");
+
+            input = keyboard.nextInt();
+            if (input == 2) {
+                System.out.print("reboot the router and try to reconnect");
+
+                System.out.println(" Did that fix the problem? 1 for yes 2 for no");
+                input = keyboard.nextInt();
+                if (input == 2) {
+                    System.out.print("Make sure the cables between routers and nodes are plugged in firmly");
+
+                    System.out.println(" Did that fix the problem? 1 for yes 2 for no");
+                    input = keyboard.nextInt();
+                    if (input == 2) {
+                        System.out.print(" Move router to new location");
+
+                        System.out.println(" Did that fix the problem? 1 for yes 2 for no");
+                        input = keyboard.nextInt();
+                        if (input == 2) {
+                            System.out.print(" Get a new router");
+                        }
+                        if (input == 1 || input == 1)
+                            System.exit(0);
+
+                    }
+                }
+
+            }
+        }
+    }
 }
